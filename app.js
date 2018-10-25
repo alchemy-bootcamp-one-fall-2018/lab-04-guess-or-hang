75fe1f6866e4c313acdc0099c69a937bacf0b508
Lise Brackbill <menagerie@hevanet.com>
2018-10-25 16:03:48 -0700

    commented in plan for html page

--- app.js
+++ app.js
@@ -2,12 +2,18 @@
 
 //building guessing game
     //function to pick word
-    
+        //randomly select index
+        //get word corresponding to index
     //set placeholder for letters
+        //evaluate word for number of letters
+        //display number of dashes corresponding to number of letters
     //program to call letters
+        //form for user to enter letter
     //evaluate if choice is correct/incorrect
-    //respond to pick again or body part
+        //if correct display letter in spot on word
+        //if incorrect increment hangman
+    //respond to pick again or decrease number of tries remaining
     //loop to evaluate man is hung
-
-
-
+        //if word completed successfully, "you win!"
+        //if man hung, "you lose!"
+        //reset to play again
