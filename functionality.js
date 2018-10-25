cc5f93190722d888611afd2ea439cc469c7b6330
Ivan <i.poblete915@gmail.com>
2018-10-25 16:27:48 -0700

    typed pseudo code

---
+++ functionality.js
@@ -0,0 +1,22 @@
+// address by index, i.e. document.getElementById('letter-' + i)
+
+// wire button up to input
+
+// letter displays on blank
+
+    // if letter is wrong display body part
+    
+    // if(guessIsWrong) {
+    // var elements = placeholder.elements;
+    // var submit = elements.guess;
+    // placeholder.textContent = '';
+    // }
+
+    // Guess Letter - A form with an input and button for guessing a letter. You can use maxlength="1" attribute on the input to limit to one letter and required attribute to require at least one letter
+    // Or you can come up with a different UI paradigm!
+    
+    // Letters Guessed - A list of letters guessed so far (correct and incorrect)
+    
+    // Number of Guesses - Shows how many total guesses have been made
+    
+    // Message - A place to show messages (like win or lose)
