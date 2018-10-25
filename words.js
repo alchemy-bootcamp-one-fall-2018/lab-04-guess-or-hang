2edf6ce769443aea12acdde162fd645c03476669
Kristin Hortsch <kristinhortsch@gmail.com>
2018-10-25 14:45:41 -0700

    initial HTML and JS files

---
+++ words.js
@@ -0,0 +1,7 @@
+/* exported */
+/* eslint-disable no-console*/
+'use strict'; 
+
+var words = ['coffee', 'rain', 'cats', 'dogs', 'random', 'function', 'array', 'pragmatic', 'horoscope', 'weather', 'sky', 'mean', 'flight', 'curry', 'courier'];
+
+console.log(words.length);
