f0d478bf6af488794f93565f1e80361e83ac7e2f
Dyanna Tolman <dyannast@gmail.com>
2018-10-25 15:20:00 -0700

    set up files and built word array

---
+++ app.js
@@ -0,0 +1,13 @@
+//placeholder for building hangman
+
+//building guessing game
+    //function to pick word
+    
+    //set placeholder for letters
+    //program to call letters
+    //evaluate if choice is correct/incorrect
+    //respond to pick again or body part
+    //loop to evaluate man is hung
+
+
+
