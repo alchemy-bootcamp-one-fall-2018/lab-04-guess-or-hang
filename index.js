d680df86c49faefd5df46819e3be5d02dceaaa36
Abel Quintero <abel.j.quintero@gmail.com>
2018-10-25 15:30:17 -0700

    added pseudocode to index.js

---
+++ index.js
@@ -0,0 +1,37 @@
+//initialize
+// generate random word
+//display number '_' for every letter in random word on screen
+//var continueGame = 'true'
+
+//while(continueGame === 'true')
+
+    //prompt user to give us a letter
+
+
+    //validate input
+
+    //check if letter is in random word
+
+    //if the letter is in the word, 
+        //display the letter for whichever spaces have that letter
+        // if they won the game
+            // continueGame = 'false'
+            // display 'you win'
+            // reveal reset button
+            // hide submit button
+            // hide textbox entry
+
+
+    //if the letter is not in the word
+        //otherwise display letter in already guessed letters
+        //add body part to hangman
+        // decrease guesses remaining by 1
+
+    //if remaining guesses = 0
+        // display words 'you lose'
+        // hide text input box
+        // hide submit button
+        // reveal reset button
+        //continueGame = 'false'
+
+
