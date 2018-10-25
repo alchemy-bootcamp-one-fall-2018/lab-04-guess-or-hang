c14650407e178cdee15c472b057180090bc1cf67
avanvalk <drewvv@gmail.com>
2018-10-25 15:17:22 -0700

    added random array

---
+++ app.js
@@ -0,0 +1,9 @@
+
+
+// var skiing = [s, k, i, i, n, g];
+
+// console.log(skiing.length);
+
+// var jogging = [j, o, g, g, i, n, g];
+
+// console.log(jogging.length);
---
+++ words.js
@@ -0,0 +1,8 @@
+
+function tryRandomWord() {
+    var words = ['skiing', 'jogging', 'aerobics', 'cycling', 'paddling'];
+
+    var rand = words[Math.floor(Math.random() * words.length)]
+    console.log(rand);
+
+}
