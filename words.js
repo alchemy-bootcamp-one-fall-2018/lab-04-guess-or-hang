8858164a8194a860c41b71d036e41c21cf70bac0
Phoebe LaBlonde <pmlablonde2220@gmail.com>
2018-10-25 16:20:59 -0700

    created underline function

--- words.js
+++ words.js
@@ -2,7 +2,20 @@
 function tryRandomWord() {
     var words = ['skiing', 'jogging', 'aerobics', 'cycling', 'paddling'];
 
-    var rand = words[Math.floor(Math.random() * words.length)]
-    console.log(rand);
+    var rand = words[Math.floor(Math.random() * words.length)];
+    console.log('random word-', rand);
+
+    return rand;
+}
+var rand = tryRandomWord();
+
+function getUnderscores(param) {
+    var wordLength = param.length;
+    console.log('word length-', wordLength);
 
 }
+
+getUnderscores(rand);
+
+
+
