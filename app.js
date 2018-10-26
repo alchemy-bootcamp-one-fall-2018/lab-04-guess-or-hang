e375f05a7b10b30d9e0b42aa2f1cf7cffd9692c1
Deborah Hardy <deb_hardy@me.com>
2018-10-26 15:43:17 -0700

    still attempting to script how many lines show on screen based on generated word length

--- app.js
+++ app.js
@@ -10,24 +10,35 @@ function getRandomWord(max) {
 
 function loadWord() {
     var copy = words.slice();   
-    console.log('random word', getRandomWord(13));
+    console.log('random word', getRandomWord(14));
 
     
     function generateWord() {
         var randomWord = getRandomWord(copy.length);
         var word = copy[randomWord];
         copy.splice(randomWord, 1);
+        
         return word;
- 
     }
 
     var selectedWord = generateWord();
     console.log(selectedWord);
 
-
-    var wordLength = selectedWord.length();
-    console.log(wordLength);
+    var answerLines = [];
+    if(copy.length === 9) {
+        selectedWord.push(copy[0]);
+    
+    }
+    for(var i = 0; i < 9; i++) {
+        var linesToHide = answerLines[i];
+        var display = '';
+        if(linesToHide) {
+            display = linesToHide();
+        }
+        console.log(answerLines);
+        
+    }
 
 } 
-// loadWord();
+loadWord();
 
