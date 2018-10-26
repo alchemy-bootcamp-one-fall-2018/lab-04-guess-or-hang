5fbfa253756f08c9558bd4f20883419ea7c0f481
Phoebe LaBlonde <pmlablonde2220@gmail.com>
2018-10-26 14:09:31 -0700

    created function to switch blanks with letter

--- words.js
+++ words.js
@@ -1,20 +1,30 @@
+var words = ['skiing', 'jogging', 'aerobics', 'cycling', 'paddling'];
 
 function tryRandomWord() {
-    var words = ['skiing', 'jogging', 'aerobics', 'cycling', 'paddling'];
 
-    var rand = words[Math.floor(Math.random() * words.length)];
+    var rand = words[Math.floor(Math.random() * words.length)].split('');
     console.log('random word-', rand);
 
     var wordLength = rand.length;
     console.log('word length-', wordLength);
 
-    var blanks = '';
-
+    var blanks = [];
     for(var i = 0; i < rand.length; i++) {
-        blanks += '_ ';
+        blanks.push('__');
         console.log(blanks);
-    } 
-    var guessInputs = document.querySelectorAll('input[name="letter"]');
+    }
+    var guessInputs = document.getElementById('input[name="letter"]');
+    for(var i = 0; i < rand.length; i++) {
+        if(guessInputs === rand[i]); 
+         blanks.splice(i, 1, rand[i]);
+        console.log(blanks);
+
+
+    }
+
+
+
+
 }
 
 
