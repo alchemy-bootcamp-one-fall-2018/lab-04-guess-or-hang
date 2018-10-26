f742f8dd9dc2aa849b64675a8a50009dbae61c62
Shaba9 <shaba92@gmail.com>
2018-10-25 17:18:00 -0700

    finished sudo coding

--- index.js
+++ index.js
@@ -8,24 +8,29 @@ function play() {
     var randomMax = words.length - 1;
     var word = words[Math.floor(Math.random() * randomMax)];
     var input = document.getElementById('guessLetter').value;
-    console.log(word, word[0]);
+    console.log(word);
     var guessCount = 0;
+    var correctGuesses = [];
 
     while(guessCount < 6) {
         
         if(word.includes(input)) {
-         //place input in correct spot of correctGuesses
-         //send message to message
+            //add input to correctGuesses array
+            //place input in correct spot of correctGuesses
+            //send message to message
+            //Give letter class of correctBlock
         } else {
             guessCount++;
             //place input in incorrectGuesses and 
             //send message to message
             // if incorrect, add body part to person
         }
+        if(correctGuesses.length === word.length) {
+            //button displays "You Win! - New Game". Reset game
+        } 
     }
     
-
-    // if number of incorrectGuesses === 6, end game and change button to a restart. Display word.
-    // if full word is guessed, button displays "You Win! New Game". Reset game
-    // adjust field boxes to adjust only so far when screen stretched.
+    // change button to a restart, button becomes "You Lose - New Game". Display "The word was --".
 }
+
+//more vertical spacing for desktop version
