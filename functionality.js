a3b88eedc68859aedf2bedd100c4e29184b2e74a
Connor Frendt <cjacksonfx@yahoo.com>
2018-10-26 10:52:34 -0700

    Drew the hangman person using js

--- functionality.js
+++ functionality.js
@@ -3,21 +3,20 @@ function tryButton() {
     console.log('hello');
 }
 
-
 // address by index, i.e. document.getElementById('letter-' + i)
 
 // wire button up to input
 
 // letter displays on blank
 
-    // if letter is wrong display body part
-    
-    // if(guessIsWrong) {
+// if letter is wrong display body part
+
+// if(guessIsWrong) {
     // var elements = placeholder.elements;
     // var submit = elements.guess;
     // placeholder.textContent = '';
     // }
-
+    
     // Guess Letter - A form with an input and button for guessing a letter. You can use maxlength="1" attribute on the input to limit to one letter and required attribute to require at least one letter
     // Or you can come up with a different UI paradigm!
     
@@ -26,3 +25,38 @@ function tryButton() {
     // Number of Guesses - Shows how many total guesses have been made
     
     // Message - A place to show messages (like win or lose)
+var circle = document.getElementById("myCanvas");
+var ctx = circle.getContext("2d");
+ctx.beginPath();
+ctx.arc(250, 100, 30, 0, 7);
+ctx.stroke();
+
+var bodyLine = document.getElementById("myCanvas");
+var ctx = bodyLine.getContext("2d");
+ctx.moveTo(250, 130);
+ctx.lineTo(250, 250);
+ctx.stroke();
+
+var leftArm = document.getElementById("myCanvas");
+var ctx = leftArm.getContext("2d");
+ctx.moveTo(250, 150);
+ctx.lineTo(180, 170);
+ctx.stroke();
+
+var rightArm = document.getElementById("myCanvas");
+var ctx = rightArm.getContext("2d");
+ctx.moveTo(250, 150);
+ctx.lineTo(320, 170);
+ctx.stroke();
+
+var leftLeg = document.getElementById("myCanvas");
+var ctx = leftLeg.getContext("2d");
+ctx.moveTo(250, 250);
+ctx.lineTo(180, 350);
+ctx.stroke();
+
+var rightLeg = document.getElementById("myCanvas");
+var ctx = rightLeg.getContext("2d");
+ctx.moveTo(250, 250);
+ctx.lineTo(320, 350);
+ctx.stroke();
