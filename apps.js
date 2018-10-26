a17977147cb4ad6828c10930f008d5a975a6d7ed
Marcy Silverman <marcysilve225@gmail.com>
2018-10-25 17:13:22 -0700

    changes

---
+++ apps.js
@@ -0,0 +1,19 @@
+// word bank
+var words = ["cheese", "coffee", "park", "flower", "book"]; 
+
+//load word randomly
+
+function loadWord() {
+
+}
+
+// choose a random word
+
+// make an array for the word
+
+// loop for guess until the word in complete
+
+// show answer until complete
+    //show congrats or lose banner
+
+// restart the game 
