57441686b3d5abd80f699ba5751717280c38dfe5
Dyanna Tolman <dyannast@gmail.com>
2018-10-25 17:52:29 -0700

    got random word to show upand characters to count

--- app.js
+++ app.js
@@ -3,27 +3,35 @@
 
 //building guessing game
     //function to pick word and bring to DOM
-// var words = document.getElementById('words');
-// // words.textContent = words.length;
+
 console.log('words', words);
-        //randomly select index
-function getRandomWord() {
-    var index = getRandomIndex(copy.length);
-    var words = copy[index];
-    copy.splice(index, 1);
-    console.log('words');
-}
-        //get word corresponding to index
+        
+    //randomly select index
+    var word = words[Math.floor(Math.random() * words.length)];
+    console.log(word);
+        
     //set placeholder for letters
+    var selectedWord = [];
+    for (var i=0; i < word.length; i++) {
+        selectedWord[i] = "_";
+    }
+
+    var remainingLetters = word.length;
+    console.log(word.length);
+
+
         //evaluate word for number of letters
-//var length = words.length;
         //display number of dashes corresponding to number of letters
+    
     //program to call letters
         //form for user to enter letter
+    
     //evaluate if choice is correct/incorrect
         //if correct display letter in spot on word
         //if incorrect increment hangman
+    
     //respond to pick again or decrease number of tries remaining
+   
     //loop to evaluate man is hung
         //if word completed successfully, "you win!"
         //if man hung, "you lose!"
