fa993c61d2271a2965816597484462137ba5dea0
JeiWolfei <jvampy22@gmail.com>
2018-10-25 17:54:14 -0700

    js setup setup

--- hangman.js
+++ hangman.js
@@ -16,4 +16,23 @@
         numLettersMatched = 0;
         currentWord = words[Math.floor(Math.random() * words.length)];
         output = document.getElementById("output");
+        man = document.getElementById("man");
+        guessInput = document.getElementById("letter");
+
+        man.innerHTML = 'You have ' + lives + ' lives remaining';
+        output.innerHTML = '';
+
+        document.getElementById("letter").value = '';
+        guessButton = document.getElementById("guess");
+
+        /* display letters in current word */
+        letters = document.getElementById("letters");
+        letters.innerHTML = '<li class="current-word">Current word:</li>';
+
+        var letter, i;
+        for (i = 0; i < currentWord.length; i++) {
+            letter = '<li class="letter letter' + currentWord.charAt(i).toUpperCase() + '">' + currentWord.charAt(i).toUpperCase() + '</li>';
+            letters.insertAdjacentHTML('beforeend', letter);
+        }
+
     }
