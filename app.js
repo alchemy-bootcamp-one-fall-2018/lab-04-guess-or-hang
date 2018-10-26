fec1231194b50697580542f214c6b7892ed69edb
Lise Brackbill <menagerie@hevanet.com>
2018-10-25 17:23:25 -0700

    successful console log

--- app.js
+++ app.js
@@ -3,13 +3,20 @@
 
 //building guessing game
     //function to pick word and bring to DOM
-var words = document.getElementById('words');
-words.textContent = words.length;
+// var words = document.getElementById('words');
+// // words.textContent = words.length;
+console.log('words', words);
         //randomly select index
+function getRandomWord() {
+    var index = getRandomIndex(copy.length);
+    var words = copy[index];
+    copy.splice(index, 1);
+    console.log('words');
+}
         //get word corresponding to index
     //set placeholder for letters
         //evaluate word for number of letters
-var length = words.length;
+//var length = words.length;
         //display number of dashes corresponding to number of letters
     //program to call letters
         //form for user to enter letter
@@ -21,3 +28,9 @@ var length = words.length;
         //if word completed successfully, "you win!"
         //if man hung, "you lose!"
         //reset to play again
+
+// based on https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+function getRandomIndex(length) {
+    //The maximum is exclusive and the minimum (0) is inclusive
+    return Math.floor(Math.random() * length); 
+}
