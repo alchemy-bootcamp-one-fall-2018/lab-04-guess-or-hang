869921e845991992136bdbf33e7b35e2e25b458c
Connor Frendt <cjacksonfx@yahoo.com>
2018-10-25 17:05:17 -0700

    played around with console and submit button

--- functionality.js
+++ functionality.js
@@ -14,7 +14,8 @@
 
     // Guess Letter - A form with an input and button for guessing a letter. You can use maxlength="1" attribute on the input to limit to one letter and required attribute to require at least one letter
     // Or you can come up with a different UI paradigm!
-    
+    var letter = document.getElementById('guessLetter')
+    console.log(guessLetter)
     // Letters Guessed - A list of letters guessed so far (correct and incorrect)
     
     // Number of Guesses - Shows how many total guesses have been made
