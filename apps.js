0616469e3a44a753f0942f540aa555ae9acdc01d
Kristin Hortsch <kristinhortsch@gmail.com>
2018-10-25 19:02:09 -0700

    still stuck on split and join

--- apps.js
+++ apps.js
@@ -3,9 +3,43 @@ var words = ["cheese", "coffee", "park", "flower", "book"];
 
 //load word randomly
 
-function loadWord() {
+var letter0 = document.getElementById('letter-0');
+var letter1 = document.getElementById('letter-1');
+var letter2 = document.getElementById('letter-2');
+var letter3 = document.getElementById('letter-3');
+var letter4 = document.getElementById('letter-4');
+var letter5 = document.getElementById('letter-5');
 
+function loadWord() {
+    var copy = words.slice();
+    
+    var index = Math.floor(Math.random() * 4)
+    console.log(index);
+
+    function getRandomWord() {
+        var word = copy[index];
+        copy.slice(index, 1);
+        return word;
+        console.log(word);
+
+    }
+
+    var selectedWord = getRandomWord();
+    //console.log(selectedWord);
+    var wordArray = selectedWord.split('');
+    console.log(wordArray);
+
+    var test = wordArray.join('');
+    var test2 = wordArray.join('-');
+
+    console.log(test2);
+
+//     for (var i = 0; i < wordArray.length; i++) {
+//         letter[i].classList.add('blank-spaces');
+//    }
 }
+
+loadWord();
 
 // choose a random word
 
