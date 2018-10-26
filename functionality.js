c7e522f59c14bef070d6b6d5ed1085e2c74f75d2
Ivan <i.poblete915@gmail.com>
2018-10-25 17:39:23 -0700

    wired button

--- functionality.js
+++ functionality.js
@@ -1,3 +1,9 @@
+
+function tryButton() {
+    console.log('hello');
+}
+
+
 // address by index, i.e. document.getElementById('letter-' + i)
 
 // wire button up to input
@@ -14,8 +20,7 @@
 
     // Guess Letter - A form with an input and button for guessing a letter. You can use maxlength="1" attribute on the input to limit to one letter and required attribute to require at least one letter
     // Or you can come up with a different UI paradigm!
-    var letter = document.getElementById('guessLetter')
-    console.log(guessLetter)
+    
     // Letters Guessed - A list of letters guessed so far (correct and incorrect)
     
     // Number of Guesses - Shows how many total guesses have been made
