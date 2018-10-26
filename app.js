736ceabb7b7ac91b3af66ed061c1fb8f7058cb8c
Dyanna Tolman <dyannast@gmail.com>
2018-10-26 13:59:14 -0700

    showing #of words in array in dom

--- app.js
+++ app.js
@@ -9,21 +9,31 @@ console.log('words', words);
     //randomly select index
     var word = words[Math.floor(Math.random() * words.length)];
     console.log(word);
-        
+    var answerDisplay = [];        
     //set placeholder for letters
-    var selectedWord = [];
-    for (var i=0; i < word.length; i++) {
-        selectedWord[i] = "_";
+    answerDisplay = word.split("");
+    console.log(answerDisplay);
+
+    //display number of dashes corresponding to number of letters
+
+    var selectedLetter = [];
+    for (var i = 0; i < word.length; i++) {
+    selectedLetter[i] = "_";
+    console.log(selectedLetter);
     }
-
-    var remainingLetters = word.length;
-    console.log(word.length);
-
-
-        //evaluate word for number of letters
-        //display number of dashes corresponding to number of letters
+    var wordLength = document.getElementById('word-length');
+    wordLength.textContent = words.length;
     
-    //program to call letters
+    //evaluate word for number of letters
+//    var remainingGuesses = word.length;
+//     console.log("There are " + word.length + " letters in the word");
+
+    //program to call letters/game loop
+   //while (remainingLetters > 0) {
+        // Show the player their progress
+     //console.log(word.join);
+   
+    
         //form for user to enter letter
     
     //evaluate if choice is correct/incorrect
@@ -38,7 +48,7 @@ console.log('words', words);
         //reset to play again
 
 // based on https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
-function getRandomIndex(length) {
-    //The maximum is exclusive and the minimum (0) is inclusive
-    return Math.floor(Math.random() * length); 
-}
+// function getRandomIndex(length) {
+//     //The maximum is exclusive and the minimum (0) is inclusive
+//     return Math.floor(Math.random() * length); 
+//images: -absolute positioning, flex box, Change img src (paint animator)
