77936fbc9b0f5ca037a000637329b7cf9730b8d9
Kristin Hortsch <kristinhortsch@gmail.com>
2018-10-27 08:01:09 -0700

    wrongTries counter works in console I think

--- apps.js
+++ apps.js
@@ -7,10 +7,12 @@ var letterDisplay = document.getElementById('blank-spaces');
 var input = document.getElementById('guess');
 var image = document.getElementById('gallows');
 var incorrectLetters = document.getElementById('letters-guessed');
+var guessNumber = document.getElementById('guess-number');
 //load word randomly
 
 var blanks = [];
 var blankJoin;
+var wrongTries = 0;
 
 function loadWord() {
     var copy = words.slice();
@@ -40,56 +42,44 @@ function handleGuess() {
     var guessedLetter = input.value.toLowerCase();
     console.log('letter guessed was', guessedLetter);
         
-    var wrongLetters = [];
-
-    var wrongTries = 0;
-    
+    var wrongLetters = [];    
     var letterInWord = false; 
-
+    
+    
     for(var i = 0; i < wordArray.length; i++) {
         if(wordArray[i] === guessedLetter) {
             console.log('match');
             letterInWord = true;  
         }
-
+        
         if(letterInWord) {
             for(var k = 0; k < wordArray.length; k++) {
                 if(wordArray[k] === guessedLetter) {
                     blanks[k] = guessedLetter;
                 }
             }
-            console.log(blanks);
             var display = blanks.join(' ');
             letterDisplay.textContent = display;
             console.log(display);
-
         }
+        
 
         else {
-            console.log('wrong');
             if(i === (wordArray.length - 1)) {
                 wrongLetters.push(guessedLetter);
-
+                var tryIncrease = true;
+                if(tryIncrease === true) {
+                    wrongTries += 1;
+                }
+                    //console.log(wrongTries.length);
+                
             }
             
-            incorrectLetters.textContent += (wrongLetters + ' ');
-
+            incorrectLetters.textContent += (wrongLetters + ' '); 
+            console.log(wrongTries);
         }
         input.value = ' ';
-
-            //wrongTries += 1;
-            //image.src = 'pic' + tries + '.png';
     }
 }
 
-
-
-
-
-        
-// loop for guess until the word is complete
-
-// show answer until complete
-    //show congrats or lose banner
-
-// restart the game 
+//image.src = 'pic' + tries + '.png';
