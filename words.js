5a4034b7cf1229eb5e313ed58628d66f0246019b
Phoebe LaBlonde <pmlablonde2220@gmail.com>
2018-10-28 14:12:14 -0700

    rewrote enterGuess function so that it doesnt loop too many times

--- words.js
+++ words.js
@@ -1,37 +1,49 @@
-/* exported enterGuess */
+/*exported enterGuess*/
 
 var words = ['skiing', 'jogging', 'aerobics', 'cycling', 'paddling'];
 
 var rand = words[Math.floor(Math.random() * words.length)].split('');
-console.log('random word-', rand);
+//console.log('random word-', rand);
 
 
 function tryRandomWord() {
     var emptyBlanks = [];
     var wordLength = rand.length;
-    console.log('word length-', wordLength);
+    //console.log('word length-', wordLength);
 
     for(var i = 0; i < wordLength; i++) {
         emptyBlanks.push('__');
-        console.log(emptyBlanks);
-
-        console.log("your word is " + wordLength + " long");
     }
+    console.log("your word is " + wordLength + " letters long");
+    console.log(emptyBlanks);
     return emptyBlanks;
 }
 var blanks = tryRandomWord();
 
 function enterGuess() {
-    console.log(blanks);
     var guessInputs = document.getElementById('userGuess').value;
-    for(var i = 0; i < rand.length; i++) {
+    if(rand.includes(guessInputs)) {
+        blanks.splice(rand.indexOf(guessInputs), 1, guessInputs);
+        console.log('you guessed correctly! Enter your next guess');
+        console.log(blanks);
+    }
+}
+
+
+
+    /*for(var i = 0; i < rand.length; i++) {
+        
         if(guessInputs === rand[i]) {
-            var userWord = blanks.splice(i, 1, rand[i]);
+            blanks.splice(i, 1, rand[i]);
+            console.log('you guessed correctly! Enter your next guess');
             console.log(blanks);
         }
-    }
+        if(guessInputs != rand[i]) {
+            console.log('TRY AGAIN!');
+        }
+    }*/
 
-}
+
     
 
 
