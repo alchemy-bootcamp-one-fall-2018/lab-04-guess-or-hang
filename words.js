4206ad84887154d58418136862146d2117fc57b4
Teonna Zaragoza <teonna.zaragoza@gmail.com>
2018-10-28 12:12:13 -0700

    refactored html

--- words.js
+++ words.js
@@ -1,3 +1,3 @@
-/* exported words */
+/* global words */
 
 var words = ['cheese', 'coffee', 'park', 'flower', 'book', 'sky', 'courier', 'soup', 'water', 'greenery', 'spoon', 'school', 'lab', 'shade', 'window'];
