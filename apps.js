e216fe333992edb468199acb15f59f25b4e759d9
Tyler Corbett <thetylercorbett@gmail.com>
2018-11-03 18:35:14 -0700

    images now swap properly

--- apps.js
+++ apps.js
@@ -1,14 +1,17 @@
 /* exported clickButton */
 var winLoseContainer = document.getElementById('win-lose-container');
 var guessLettersForm = document.getElementById('guess-letters');
+const guessedLetterDisplay = document.getElementById('letter-display');
+
 
 var wordList = ['heart', 'puppy', 'kitten', 'octopus', 'platypus', 'diamond', 'gold', 'river'];
 var randomWord = wordList[Math.floor(Math.random() * wordList.length)];
 var wordLength = randomWord.length;
 var randomWordArray = randomWord.split('');
 var displayGuesses = [];
-var guessCount = 6;
+var guessCount = 0;
 var gameIsDone = false;
+let image = document.getElementById('gallows');
 
 
 for(var i = 0; i < wordLength; i++) {
@@ -50,8 +53,9 @@ function checkGuess(guessedLetter) {
         }
     }
     if(letterIsInWord === false) {
-        guessCount = guessCount - 1;
-        if(guessCount === 0) {
+        guessCount = guessCount + 1;
+        image.src = './hangman-images/' + guessCount + '.jpg';
+        if(guessCount === 6) {
             gameIsDone = true;
             winLoseContainer.textContent = 'You lose!';
         }
@@ -66,48 +70,3 @@ function clickButton() {
         console.log(guess, randomWordArray, displayGuesses);
     }
 }
-
-
-
-
-
-
-
-
-
-//initialize DONE
-// generate random word   DONE
-//display number '_' for every letter in random word on screen DONE
-//var continueGame = 'true'
-
-//while(continueGame === 'true')
-
-    //prompt user to give us a letter
-
-
-    //validate input
-
-    //check if letter is in random word
-
-    //if the letter is in the word, 
-        //display the letter for whichever spaces have that letter
-        // if they won the game
-            // continueGame = 'false'
-            // display 'you win'
-            // reveal reset button
-            // hide submit button
-            // code-spell-checker:disable-next-line
-            // hide textbox entry
-
-
-    //if the letter is not in the word
-        //otherwise display letter in already guessed letters
-        //add body part to hangman
-        // decrease guesses remaining by 1
-
-    //if remaining guesses = 0
-        // display words 'you lose'
-        // hide text input box
-        // hide submit button
-        // reveal reset button
-        //continueGame = 'false'
